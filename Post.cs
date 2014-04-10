abe8e2d997eaf14430260ba0cd3529cbf0ee1dbf
solutionyogi <contact@solutionyogi.com>
2014-04-09 23:18:33 -0400

    Refactoring code to move logic to the Post class.

--- Post.cs
+++ Post.cs
@@ -1,10 +1,16 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using HtmlAgilityPack;
+using NLog;
 
 namespace MSDNBlogParser
 {
     public class Post
     {
+        private static readonly Logger Logger = LogManager.GetCurrentClassLogger();
+
         public DateTime Date { get; set; }
 
         public string Contents { get; set; }
@@ -15,10 +21,126 @@ namespace MSDNBlogParser
 
         public string Title { get; set; }
 
+        private HtmlDocument MainHtmlDocument { get; set; }
+
+        private IEnumerable<string> CommentHtmlFilePathList { get; set; }
+
+        private string MainHtmlFilePath { get; set; }
+
+        private HtmlNode MainPostNode { get; set; }
+
         public Post()
         {
             Tags = new List<string>();
             Comments = new List<Comment>();
+        }
+
+        public static Post Create(string mainHtmlFilePath, IEnumerable<string> commentHtmlFilePathList)
+        {
+            if(string.IsNullOrWhiteSpace(mainHtmlFilePath))
+                throw new ArgumentNullException("mainHtmlFilePath");
+
+            if(commentHtmlFilePathList == null)
+                throw new ArgumentNullException("commentHtmlFilePathList");
+
+            if(! File.Exists(mainHtmlFilePath))
+                throw new InvalidOperationException(string.Format("File specified by path {0} doesn't exist.", mainHtmlFilePath));
+
+            var post = new Post
+                       {
+                           MainHtmlFilePath = mainHtmlFilePath,
+                           CommentHtmlFilePathList = commentHtmlFilePathList
+                       };
+
+            post.ParseHtmlDocument();
+            return post;
+        }
+
+        private void ParseHtmlDocument()
+        {
+            MainHtmlDocument = GetHtmlDocument(MainHtmlFilePath);
+
+            MainPostNode = MainHtmlDocument.DocumentNode.SelectSingleNode("//div[@class='full-post']");
+
+            ParseDate();
+
+            ParseTitle();
+
+            ParseContents();
+
+            ParseTags();
+
+            ParseComments();
+        }
+
+        private IEnumerable<HtmlDocument> HtmlDocumentList
+        {
+            get
+            {
+                yield return MainHtmlDocument;
+
+                foreach(var htmlFilePath in CommentHtmlFilePathList)
+                {
+                    yield return GetHtmlDocument(htmlFilePath);
+                }
+            }
+        }
+
+        private void ParseComments()
+        {
+            Comments.AddRange(HtmlDocumentList.SelectMany(ParseComments));
+        }
+
+        private static HtmlDocument GetHtmlDocument(string htmlFilePath)
+        {
+            var document = new HtmlDocument();
+            document.Load(htmlFilePath);
+            return document;
+        }
+
+        private static IEnumerable<Comment> ParseComments(HtmlDocument document)
+        {
+            var mainCommentsNode = document.DocumentNode.SelectSingleNode("//div[contains(@class, 'blog-feedback-list')]");
+            var unorderedListNode = mainCommentsNode.SelectSingleNode(".//ul[@class='content-list']");
+            var listItemNodes = unorderedListNode.SelectNodes("li");
+            return listItemNodes.Select(listItemNode => new Comment
+                                                        {
+                                                            Author =
+                                                                listItemNode.SelectSingleNode(".//div[@class='post-author']/*/span[@class='user-name']")
+                                                                .InnerText,
+                                                            Date = DateTime.Parse(listItemNode.SelectSingleNode(".//div[@class='post-date']/span").InnerText),
+                                                            Contents = listItemNode.SelectSingleNode(".//div[contains(@class, 'post-content')]").InnerText
+                                                        });
+        }
+
+        private void ParseTags()
+        {
+            var tagsNode = MainPostNode.SelectSingleNode("div[@class='post-tags']");
+
+            var tagLinks = tagsNode.SelectNodes(".//a[@rel='tag']");
+
+            foreach(var tagLink in tagLinks)
+            {
+                Tags.Add(tagLink.InnerText);
+            }
+        }
+
+        private void ParseContents()
+        {
+            Contents = MainPostNode.SelectSingleNode(".//div[@class='mine']").InnerText;
+        }
+
+        private void ParseTitle()
+        {
+            Title = MainPostNode.SelectSingleNode("h3[@class='post-name']").InnerText;
+        }
+
+        private void ParseDate()
+        {
+            var dateNode = MainPostNode.SelectSingleNode("div[@class='post-date']");
+            var dateAsText = dateNode.SelectSingleNode("span").InnerText;
+
+            Date = DateTime.Parse(dateAsText);
         }
     }
 }
--- Program.cs
+++ Program.cs
@@ -1,15 +1,13 @@
 ﻿using System;
-using System.Collections.Generic;
 using System.IO;
 using System.Linq;
 using System.Net;
-using HtmlAgilityPack;
 
 namespace MSDNBlogParser
 {
-    class Program
+    internal class Program
     {
-        static void Main(string[] args)
+        private static void Main(string[] args)
         {
             var post = GetPost("http://blogs.msdn.com/b/ericlippert/archive/2012/11/29/a-new-fabulous-adventure.aspx");
             Console.WriteLine(post.Date);
@@ -20,71 +18,12 @@ namespace MSDNBlogParser
 
         private static Post GetPost(string url)
         {
-            var filePath = GetPageContents(url);
-            var document = new HtmlDocument();
-            document.Load(filePath);
-
-            var fullPostNode = document.DocumentNode.SelectSingleNode("//div[@class='full-post']");
-
-            var result = new Post();
-
-            var dateNode = fullPostNode.SelectSingleNode("div[@class='post-date']");
-            var dateAsText = dateNode.SelectSingleNode("span").InnerText;
-
-            result.Date = DateTime.Parse(dateAsText);
-            result.Title = fullPostNode.SelectSingleNode("h3[@class='post-name']").InnerText;
-            result.Contents = fullPostNode.SelectSingleNode(".//div[@class='mine']").InnerText;
-
-            var tagsNode = fullPostNode.SelectSingleNode("div[@class='post-tags']");
-
-            var tagLinks = tagsNode.SelectNodes(".//a[@rel='tag']");
-
-            foreach(var tagLink in tagLinks)
-            {
-                result.Tags.Add(tagLink.InnerText);
-            }
-
-            //Comments on the main page.
-            result.Comments.AddRange(ParseComments(document));
-
-            var pagerNode = document.DocumentNode.SelectSingleNode("//div[@class='pager']");
-
-            var links = pagerNode.SelectNodes(".//a");
-
-            foreach(var link in links)
-            {
-                const string baseUrl = "http://blogs.msdn.com";
-                var fullUrl = baseUrl + link.Attributes["href"].Value;
-                Console.WriteLine("Parsing comment page {0}", fullUrl);
-                var pageDocument = GetHtmlDocument(fullUrl);
-                result.Comments.AddRange(ParseComments(pageDocument));
-            }
-
-            return result;
+            var filePath = GetHtmlPage(url);
+            var post = Post.Create(filePath, Enumerable.Empty<string>());
+            return post;
         }
 
-        private static HtmlDocument GetHtmlDocument(string fullUrl)
-        {
-            var filePath = GetPageContents(fullUrl);
-            var document = new HtmlDocument();
-            document.Load(filePath);
-            return document;
-        }
-
-        private static IEnumerable<Comment> ParseComments(HtmlDocument document)
-        {
-            var mainCommentsNode = document.DocumentNode.SelectSingleNode("//div[contains(@class, 'blog-feedback-list')]");
-            var unorderedListNode = mainCommentsNode.SelectSingleNode(".//ul[@class='content-list']");
-            var listItemNodes = unorderedListNode.SelectNodes("li");
-            return listItemNodes.Select(listItemNode => new Comment
-                                                        {
-                                                            Author = listItemNode.SelectSingleNode(".//div[@class='post-author']/*/span[@class='user-name']").InnerText,
-                                                            Date = DateTime.Parse(listItemNode.SelectSingleNode(".//div[@class='post-date']/span").InnerText),
-                                                            Contents = listItemNode.SelectSingleNode(".//div[contains(@class, 'post-content')]").InnerText
-                                                        });
-        }
-
-        private static string GetPageContents(string url)
+        private static string GetHtmlPage(string url)
         {
             var tempFileName = Path.GetTempFileName();
             var client = new WebClient();
